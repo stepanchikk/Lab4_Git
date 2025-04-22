12cfd43b38d5d7fb12f39936b66b240cfbdb86c0
stepanchikk <122825623+stepanchikk@users.noreply.github.com>
2025-04-22 23:39:21 +0300

    add main

---
+++ GitLab4/Calculator(main)/Calculator(main)/Calculator(main).cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include "math_operations.h"
+
+int main() {
+    int x, y;
+    std::cout << "Enter two integers: ";
+    std::cin >> x >> y;
+
+    std::cout << "Sum: " << add(x, y) << std::endl;
+    std::cout << "Difference: " << subtract(x, y) << std::endl;
+    std::cout << "Product: " << multiply(x, y) << std::endl;
+
+    return 0;
+}
