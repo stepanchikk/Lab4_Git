f76ed0c971c09c7e75da77e2ec14c42d36372cc7
sandykdanik17 <danik2007.ds10@gmail.com>
2025-05-01 11:31:46 +0300

    function-part added

---
+++ chokan-part/chokan-part/chokan-part.cpp
@@ -0,0 +1,21 @@
+﻿#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+class Student {
+private:
+    std::string name;
+    int attendance;
+    std::vector<int> grades;
+
+public:
+    Student();
+    void inputManual();
+    void inputRandom();
+    void display() const;
+};
+
+#endif // STUDENT_H
